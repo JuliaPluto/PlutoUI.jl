6a3c48fd40485c1fe3b349dea8bbcaa26dfb998f
fonsp <fonsvdplas@gmail.com>
2020-10-02 23:55:31 +0000

    add start_running property

--- assets/clock.js
+++ assets/clock.js
@@ -18,7 +18,6 @@ tpsInput.oninput = (e) => {
     analogfront.style.animationDuration = dt + "s"
     e && e.stopPropagation()
 }
-clock.classList.toggle("stopped")
 tpsInput.oninput()
 
 analogfront.onanimationiteration = (e) => {
