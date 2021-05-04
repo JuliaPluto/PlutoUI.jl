0ced20ef1e8ba2215db9f5d43310632fbbc6fe4e
Michael Pusterhofer <michael.pusterhofer@ams.com>
2021-05-04 14:15:26 +0200

    fix typo in examples

--- src/CSSWidgetWrapper.jl
+++ src/CSSWidgetWrapper.jl
@@ -8,11 +8,11 @@ Modify the style properties of the first HTML node for the given widget.
 # Examples
 Generate a text field with a red background and white text.
 ```
-@bind text CSSWidgetWrapper(Textfield(),Dict("backgroundColor"=>"red","color"=>"white"))
+@bind text CSSWidgetWrapper(TextField(),Dict("backgroundColor"=>"red","color"=>"white"))
 ```
 Generate a text field with 1px wide solid red border.
 ```
-@bind text CSSWidgetWrapper(Textfield(),Dict("border"=>"1px solid red"))
+@bind text CSSWidgetWrapper(TextField(),Dict("border"=>"1px solid red"))
 ```
 """
 struct CSSWidgetWrapper
