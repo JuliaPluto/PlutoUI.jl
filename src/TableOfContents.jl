820b8356063528ea973aa7a0744078743c926fcf
Fons van der Plas <fonsvdplas@gmail.com>
2021-10-06 15:22:04 +0200

    Update TableOfContents.jl

--- src/TableOfContents.jl
+++ src/TableOfContents.jl
@@ -259,13 +259,6 @@ TableOfContents(; aside = false)
 
 # ╔═╡ 06ac2f13-e1f7-477a-9b3c-4d8545b777d9
 
-[[Base64]]
-uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
-
-[[Markdown]]
-deps = ["Base64"]
-uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
-"""
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
