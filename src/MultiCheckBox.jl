978ee3eea1c10ba8646b503b32b4d9439ba215e0
Fons van der Plas <fonsvdplas@gmail.com>
2021-12-17 21:55:32 +0100

    update docs

--- src/MultiCheckBox.jl
+++ src/MultiCheckBox.jl
@@ -56,25 +56,46 @@ subarrays(x) = (
 begin
 	export MultiCheckBox
     local result = begin
-    """A group of checkboxes (`<input type="checkbox">`) - the user can choose enable or disable of the `options`, an array of `Strings`.
+    """
+    ```julia
+    MultiCheckBox(options::Vector; [default::Vector], [orientation ∈ [:row, :column]], [select_all::Bool])
+    ```
+    
+    A group of checkboxes - the user can choose which of the `options` to return.
     The value returned via `@bind` is a list containing the currently checked items.
 
     See also: [`MultiSelect`](@ref).
 
-    `options` can also be an array of pairs `key::String => value::Any`. The `key` is returned via `@bind`; the `value` is shown.
+    `options` can also be an array of pairs `key::Any => value::String`. The `key` is returned via `@bind`; the `value` is shown.
 
-    `defaults` specifies which options should be checked initally.
-
-    `orientation` specifies whether the options should be arranged in `:row`'s `:column`'s.
-
-    `select_all` specifies whether or not to include a "Select All" checkbox.
+    # Keyword arguments
+    - `defaults` specifies which options should be checked initally.
+    - `orientation` specifies whether the options should be arranged in `:row`'s `:column`'s.
+    - `select_all` specifies whether or not to include a "Select All" checkbox.
 
     # Examples
-    `@bind snacks MultiCheckBox(["🥕", "🐟", "🍌"]))`
+    ```julia
+    @bind snacks MultiCheckBox(["🥕", "🐟", "🍌"]))
+    
+    if "🥕" ∈ snacks
+        "Yum yum!"
+    end
+    ```
+    
+    ```julia
+    @bind functions MultiCheckBox([sin, cos, tan])
+    
+    [f(0.5) for f in functions]
+    ```
 
-    `@bind snacks MultiCheckBox(["🥕" => "🐰", "🐟" => "🐱", "🍌" => "🐵"]; default=["🥕", "🍌"])`
+    ```julia
+    @bind snacks MultiCheckBox(["🥕" => "🐰", "🐟" => "🐱", "🍌" => "🐵"]; default=["🥕", "🍌"])
+    ```
 
-    `@bind animals MultiCheckBox(["🐰", "🐱" , "🐵", "🐘", "🦝", "🐿️" , "🐝",  "🐪"]; orientation=:column, select_all=true)`"""
+    ```julia
+    @bind animals MultiCheckBox(["🐰", "🐱" , "🐵", "🐘", "🦝", "🐿️" , "🐝",  "🐪"]; orientation=:column, select_all=true)
+    ```
+    """
     struct MultiCheckBox{BT,DT}
         options::AbstractVector{Pair{BT,DT}}
         default::Union{Missing,AbstractVector{BT}}
