790026ca875b4fbfabd15a1f2c3fb44e4ef5d0ff
Fons van der Plas <fonsvdplas@gmail.com>
2022-01-24 22:52:50 +0100

    typo

--- src/Builtins.jl
+++ src/Builtins.jl
@@ -862,9 +862,9 @@ Use `default` to set the initial value.
 	This is `DateField`, but you should use our new function, [`DatePicker`](@ref), which is much better! It returns a `Date` directly, instead of a `DateTime`.
 
 # Examples
-`@bind best_day_of_my_live DateField()`
+`@bind best_day_of_my_life DateField()`
 
-`@bind best_day_of_my_live DateField(default=today())`
+`@bind best_day_of_my_life DateField(default=today())`
 
 # See also
 [`DatePicker`](@ref)
