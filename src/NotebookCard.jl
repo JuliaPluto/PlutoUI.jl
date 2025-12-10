5be37d981595ebd3e6a3f2108079adf7de60c7b6
Fons van der Plas <fonsvdplas@gmail.com>
2025-12-10 17:25:52 +0100

    NotebookCard: implement in JS to avoid PlutoSliderServer caching (#329)

--- src/NotebookCard.jl
+++ src/NotebookCard.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.14
+# v0.20.21
 
 using Markdown
 using InteractiveUtils
@@ -9,64 +9,6 @@ using HypertextLiteral
 
 # ╔═╡ 52147b93-5178-4041-aabd-5ffc629ad191
 
-
-# ╔═╡ 97b04c4e-d9a7-4862-98e0-ad7b36e85181
-import JSON
-
-# ╔═╡ 3da3063a-a99c-4db9-8f94-796104ecddfd
-import Downloads
-
-# ╔═╡ 74ee1aef-defb-41d0-85ab-40a76481b3dc
-function downloads_get(args...; kwargs...)
-	local resp
-	s = sprint() do io
-		resp = Downloads.request(args...; kwargs..., output=io, throw=false)
-	end
-	s, resp
-end
-
-# ╔═╡ 2182628d-2ce5-4486-8dd3-ddf52694b5bc
-import URIs
-
-# ╔═╡ 1eda68a2-cef6-4f58-a7d0-068a731ae8b8
-function find_root_json(notebook_html_url)
-	url = URIs.URI(notebook_html_url)
-	path_parts = split(url.path, "/")
-	for i in eachindex(path_parts)
-		newpath = join([path_parts[begin:i]..., "pluto_export.json"], "/")
-		newurl = URIs.URI(url; path=newpath)
-
-		htmlpath_relative = join(path_parts[i+1:end], "/")
-
-		data, resp = downloads_get(string(newurl))
-		
-		if resp.status ∈ 200:299
-			data_parsed = JSON.parse(data)
-			nbs = data_parsed["notebooks"]
-
-			clean(x) = replace(lowercase(x), r"(index)?\.html$" => "")
-			matches_html = filter(nbs) do (_jl, nbdata)
-				clean(URIs.escapepath(nbdata["html_path"])) == clean(htmlpath_relative)
-			end
-
-			if length(matches_html) == 0
-				error("The `pluto_export.json` was found, but this notebook does not appear in the json file. Weird! Are you sure that the notebook URL is correct?")
-			end
-
-			jlpath_relative = only(keys(matches_html))
-			notebook_data = only(values(matches_html))
-			
-			return (
-				json_url=newurl,
-				json_data=data_parsed,
-				htmlpath_relative,
-				jlpath_relative,
-				notebook_data,
-			)
-		end
-	end
-	error("Not found. Are you sure that this website is generated with PlutoSliderServer or PlutoPages?")
-end
 
 # ╔═╡ 6f8c5856-bf1a-4aa8-b9e1-fe58fb11f811
 const white_svg_uri = """data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100'><rect width='100%' height='100%' fill='ivory'/></svg>"""
@@ -99,24 +41,15 @@ This is what it looks like in a notebook:
 
 """
 function NotebookCard(notebook_url; link_text = "Read article")
-	if URIs.URI(notebook_url).host == "pluto.land"
-		error("Displaying cards for pluto.land notebooks is not yet supported!")
+	if !startswith(notebook_url, r"https?://")
+		throw(ArgumentError("Notebook URL should start with https://"))
 	end
 	href = notebook_url
-	json_result = try
-		find_root_json(href)
-	catch
-		@error "This only works for notebooks hosted on websites that are generated with PlutoPages.jl or PlutoSliderServer.jl."
-		rethrow()
-	end
-	frontmatter = let
-		json_result.notebook_data["frontmatter"]
-	end
-
 	
-	image_url = get(frontmatter, "image", white_svg_uri)
-	title = get(frontmatter, "title", nothing)
-	description = get(frontmatter, "description", nothing)
+	# Initial placeholder values - will be replaced by JavaScript
+	image_url = white_svg_uri
+	title = "Loading..."
+	description = ""
 	
 	
 	@htl(
@@ -135,20 +68,25 @@ function NotebookCard(notebook_url; link_text = "Read article")
 	</div>
 
 	<script>
-		const json_url = $(json_result.json_url |> string)
-		const jlpath_relative = $(json_result.jlpath_relative)
+		const notebook_html_url = $(href)
+		const white_svg_uri = $white_svg_uri
 
-		const json_data = await (await fetch(json_url)).json()
-		console.log(json_data)
+		const html_data = await fetch(notebook_html_url).then(r => r.text()).catch(e => "")
+		const doc = new DOMParser().parseFromString(html_data, "text/html");
+		const head = doc.head
 
-		const frontmatter = json_data.notebooks[jlpath_relative].frontmatter
-		console.log(frontmatter)
-
+		console.log({doc, notebook_html_url})
 		const q = sel => currentScript.parentElement.querySelector(".pe-card").querySelector(sel)
 
-		q("a img").src = frontmatter.image ?? $white_svg_uri
-		q(".pe-about h2").innerText = frontmatter.title ?? "Untitled"
-		q(".pe-about p").innerText = frontmatter.description ?? ""
+		q("a img").src = head.querySelector('meta[property="og:image"]')?.content ??
+			white_svg_uri
+		
+		q(".pe-about h2").innerText = (doc.title == "" ? null : doc.title) ?? 
+			new URL(notebook_html_url).pathname.split("/").map(decodeURIComponent).toReversed().find(s => s) ??
+			"Notebook"
+		
+		q(".pe-about p").innerText = head.querySelector('meta[name="description"]')?.content ?? 
+			""
 	</script>
 
 	<style>
@@ -164,12 +102,11 @@ function NotebookCard(notebook_url; link_text = "Read article")
 
 			border-radius: 6px;
 			background: salmon;
-		background: linear-gradient(80deg, #e9dbe3, #ffdabe);
-	padding: 10px;
-	gap: 20px;
-	margin: 10px 0;
-	box-shadow: 0 2px 10px rgb(0 0 0 / 14%);
-
+			background: linear-gradient(80deg, #e9dbe3, #ffdabe);
+			padding: 10px;
+			gap: 20px;
+			margin: 10px 0;
+			box-shadow: 0 2px 10px rgb(0 0 0 / 14%);
 		}
 
 		@container (max-width: 400px) {
@@ -177,9 +114,9 @@ function NotebookCard(notebook_url; link_text = "Read article")
 				flex-direction: column;
 				gap: 0;
 			}
-		.pe-about {
-			margin: 0 10px 10px 10px;
-		}
+			.pe-about {
+				margin: 0 10px 10px 10px;
+			}
 		}
 
 		.pe-card > a {
@@ -202,8 +139,6 @@ function NotebookCard(notebook_url; link_text = "Read article")
 			text-decoration: none;
 			border-bottom: none;
 			margin-block-end: 0;
-		}
-		div.pe-about h2 {
 		}
 
 		.pe-right {
@@ -251,7 +186,23 @@ NotebookCard("https://featured.plutojl.org/basic/basic%20mathematics"; link_text
 # ╔═╡ a78fd4f9-c671-4e50-af4b-c7b1b3c5db40
 # ╠═╡ skip_as_script = true
 #=╠═╡
-NotebookCard("https://pluto.land/n/cibk2zp8")
+NotebookCard("http://pluto.land/n/cibk2zp8")
+  ╠═╡ =#
+
+# ╔═╡ dffaee00-8d1c-42ba-96ea-01d153c30660
+# ╠═╡ skip_as_script = true
+#=╠═╡
+try
+	NotebookCard("pluto.land/n/cibk2zp8")
+catch e
+	"yay", e
+end
+  ╠═╡ =#
+
+# ╔═╡ 0a56a6cc-cbf3-4dd6-bfaa-8ab242ef4012
+# ╠═╡ skip_as_script = true
+#=╠═╡
+NotebookCard("https://asdf.com")
   ╠═╡ =#
 
 # ╔═╡ 5aa2e75b-86c8-420f-a35e-7463031b45bd
@@ -263,43 +214,19 @@ NotebookCard("https://pluto.land/n/cibk2zp8")
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
-Downloads = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
 HypertextLiteral = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
-JSON = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
-URIs = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
 
 [compat]
 HypertextLiteral = "~0.9.5"
-JSON = "~0.21.4"
-URIs = "~1.5.2"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.10.9"
+julia_version = "1.12.2"
 manifest_format = "2.0"
-project_hash = "9032b5d55a17c263eedc82b9e91ed00863a30db8"
-
-[[deps.ArgTools]]
-uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
-version = "1.1.1"
-
-[[deps.Artifacts]]
-uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
-
-[[deps.Dates]]
-deps = ["Printf"]
-uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
-
-[[deps.Downloads]]
-deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
-uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
-version = "1.6.0"
-
-[[deps.FileWatching]]
-uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
+project_hash = "de8c559f3dd69bd823c8bc954861ad3089248731"
 
 [[deps.HypertextLiteral]]
 deps = ["Tricks"]
@@ -307,107 +234,10 @@ git-tree-sha1 = "7134810b1afce04bbc1045ca1985fbe81ce17653"
 uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
 version = "0.9.5"
 
-[[deps.JSON]]
-deps = ["Dates", "Mmap", "Parsers", "Unicode"]
-git-tree-sha1 = "31e996f0a15c7b280ba9f76636b3ff9e2ae58c9a"
-uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
-version = "0.21.4"
-
-[[deps.LibCURL]]
-deps = ["LibCURL_jll", "MozillaCACerts_jll"]
-uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
-version = "0.6.4"
-
-[[deps.LibCURL_jll]]
-deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
-uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
-version = "8.4.0+0"
-
-[[deps.LibSSH2_jll]]
-deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
-uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
-version = "1.11.0+1"
-
-[[deps.Libdl]]
-uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
-
-[[deps.MbedTLS_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
-version = "2.28.2+1"
-
-[[deps.Mmap]]
-uuid = "a63ad114-7e13-5084-954f-fe012c677804"
-
-[[deps.MozillaCACerts_jll]]
-uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
-version = "2023.1.10"
-
-[[deps.NetworkOptions]]
-uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
-version = "1.2.0"
-
-[[deps.Parsers]]
-deps = ["Dates", "PrecompileTools", "UUIDs"]
-git-tree-sha1 = "7d2f8f21da5db6a806faf7b9b292296da42b2810"
-uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
-version = "2.8.3"
-
-[[deps.PrecompileTools]]
-deps = ["Preferences"]
-git-tree-sha1 = "5aa36f7049a63a1528fe8f7c3f2113413ffd4e1f"
-uuid = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
-version = "1.2.1"
-
-[[deps.Preferences]]
-deps = ["TOML"]
-git-tree-sha1 = "9306f6085165d270f7e3db02af26a400d580f5c6"
-uuid = "21216c6a-2e73-6563-6e65-726566657250"
-version = "1.4.3"
-
-[[deps.Printf]]
-deps = ["Unicode"]
-uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
-
-[[deps.Random]]
-deps = ["SHA"]
-uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
-
-[[deps.SHA]]
-uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
-version = "0.7.0"
-
-[[deps.TOML]]
-deps = ["Dates"]
-uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
-version = "1.0.3"
-
 [[deps.Tricks]]
 git-tree-sha1 = "6cae795a5a9313bbb4f60683f7263318fc7d1505"
 uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
 version = "0.1.10"
-
-[[deps.URIs]]
-git-tree-sha1 = "cbbebadbcc76c5ca1cc4b4f3b0614b3e603b5000"
-uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
-version = "1.5.2"
-
-[[deps.UUIDs]]
-deps = ["Random", "SHA"]
-uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
-
-[[deps.Unicode]]
-uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
-
-[[deps.Zlib_jll]]
-deps = ["Libdl"]
-uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
-version = "1.2.13+1"
-
-[[deps.nghttp2_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
-version = "1.52.0+1"
 """
 
 # ╔═╡ Cell order:
@@ -415,14 +245,11 @@ version = "1.52.0+1"
 # ╠═96cd7388-02f6-4d59-9243-83c48c8b6098
 # ╠═b504b24e-dd50-4f49-9756-f4cd5c7b7934
 # ╠═a78fd4f9-c671-4e50-af4b-c7b1b3c5db40
+# ╠═dffaee00-8d1c-42ba-96ea-01d153c30660
+# ╠═0a56a6cc-cbf3-4dd6-bfaa-8ab242ef4012
 # ╟─52147b93-5178-4041-aabd-5ffc629ad191
 # ╠═ececef64-34d7-4697-ad13-0e014d8fa1fa
-# ╟─74ee1aef-defb-41d0-85ab-40a76481b3dc
-# ╟─1eda68a2-cef6-4f58-a7d0-068a731ae8b8
 # ╠═1a3f2d74-4a9d-11f0-01a5-7db63f85481f
-# ╠═97b04c4e-d9a7-4862-98e0-ad7b36e85181
-# ╠═3da3063a-a99c-4db9-8f94-796104ecddfd
-# ╠═2182628d-2ce5-4486-8dd3-ddf52694b5bc
 # ╠═6f8c5856-bf1a-4aa8-b9e1-fe58fb11f811
 # ╠═5aa2e75b-86c8-420f-a35e-7463031b45bd
 # ╟─00000000-0000-0000-0000-000000000001
