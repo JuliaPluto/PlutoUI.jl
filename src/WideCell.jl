bd341811836e004b3922e9edd1a6c12ec877df07
Fons van der Plas <fonsvdplas@gmail.com>
2026-05-19 10:40:32 +0200

    Update WideCell.jl

--- src/WideCell.jl
+++ src/WideCell.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.8
+# v0.20.26
 
 using Markdown
 using InteractiveUtils
@@ -19,13 +19,51 @@ PlutoUI.TableOfContents()
   ╠═╡ =#
 
 # ╔═╡ 376bc398-a982-4b01-bc2a-e894d15893ae
-
+import AbstractPlutoDingetjes
 
 # ╔═╡ f5882dbd-a603-4df8-8a20-3b2b34f0cf84
 const DEFAULT_WIDTH = 1000
 
 # ╔═╡ 4edba64f-4e71-4780-964a-d8635470a595
 WideCell(; kwargs...) = x -> WideCell(x; kwargs...)
+
+# ╔═╡ 8664c054-1cf5-41bb-9f86-c1ab408b1110
+begin
+	struct SafeReactDOMElement
+		tag
+		children
+		attributes
+	end
+
+	# This one will take preference, if supported.
+	function Base.show(io::IO, m::MIME"application/vnd.pluto.reactdomelement+object", s::SafeReactDOMElement)
+		return AbstractPlutoDingetjes.Display.ReactDOMElement(
+			tag=s.tag,
+			children=s.children,
+			attributes=s.attributes,
+		)
+	end
+
+	# The fallback
+	function Base.show(io::IO, m::MIME"text/html", s::SafeReactDOMElement)
+		# Check if the old DivElement exists...
+		if isdefined(Main, :PlutoRunner) && isdefined(Main.PlutoRunner, :DivElement) && AbstractPlutoDingetjes.is_inside_pluto(io)
+			de = Main.PlutoRunner.DivElement(
+				children=s.children, 
+				class=get(s.attributes, :class, get(s.attributes, "class", nothing)),
+				style=get(s.attributes, :style, get(s.attributes, "style", "")),
+			)
+
+			show(io, m, de)
+		else
+			@warn """PlutoUI.WideCell failed to communicate with Pluto. This means that either:
+			- This version of PlutoUI is not compatible with this version of Pluto. Try upgrading both to the latest versions.
+			- WideCell is used in an unsupported way, e.g. embedded in another element.
+			- WideCell is used outside of Pluto."""
+			show(io, m, @htl """<$(s.tag) $(s.attributes)...>$(s.children)</$(s.tag)>""")
+		end
+	end
+end
 
 # ╔═╡ 99246dda-6df5-4f3e-adeb-9a3976c4bb51
 """
@@ -148,18 +186,28 @@ function WideCell(x; max_width::Int64=DEFAULT_WIDTH)
 			})
 		</script>"""),
 	]
-	
-	# Normally, you would just use PlutoUI.ExperimentalLayout.Div here
-	# But because this is inside the source code of PlutoUI I am taking a shortcut
-	Main.PlutoRunner.DivElement(; children, class="pui-big-wrapper")
+
+	SafeReactDOMElement("div", children, Dict(:class => "pui-big-wrapper"))
 end
 
 # ╔═╡ bd9e8715-4f03-43f7-b279-3d1f68365656
+#=╠═╡
 export WideCell
+  ╠═╡ =#
+
+# ╔═╡ 72a735ab-41bd-4ef3-ab2a-a019d9608731
+let
+	tag = "marquee"
+	attributed = Dict(:style => "color: red;", "class"=>"asdfdfs")
+	children = [1,2,3]
+
+	@htl """<$(tag) $(attributed)...>$(children)</$(tag)>"""
+end
 
 # ╔═╡ 88d541c1-4e3f-4852-a58a-610a184f5e61
 # ╠═╡ skip_as_script = true
 #=╠═╡
+# The point of this redirect is so we can "Disable in File" this cell, and every test will be disabled automatically.
 const WideCellTest = WideCell
   ╠═╡ =#
 
@@ -245,47 +293,49 @@ wide_text = WideCellTest(md"hello", max_width=2000)
 [md"hello", md"hello"]
   ╠═╡ =#
 
-# ╔═╡ 97b64bd7-49d0-4f7f-908d-06848abe6ab7
+# ╔═╡ bd860523-ec6e-41f0-90da-6f8a5b627d9e
 md"""
-# asdf
-
-
-## asdfsdfdsf
-
-### asdfadsf
-
-## asdfdsffd
-
-## asdf
-
-
-# adsfsdf
+# Test fallback display
 """
+
+# ╔═╡ b681cfc2-a5f1-4d1f-942f-3847d8ddf58a
+# ╠═╡ skip_as_script = true
+#=╠═╡
+hhh(x) = sprint() do io
+	show(io, MIME"text/html"(), x)
+end |> HTML
+  ╠═╡ =#
+
+# ╔═╡ ddde2dcb-e280-4c7d-ae50-f624222e6a41
+#=╠═╡
+hhh(WideCell([1,2,3, 999]))
+  ╠═╡ =#
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
+AbstractPlutoDingetjes = "6e696c72-6542-2067-7265-42206c756150"
 HypertextLiteral = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
 PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
 
 [compat]
-HypertextLiteral = "~0.9.5"
-PlutoUI = "~0.7.59"
+AbstractPlutoDingetjes = "~1.4.0"
+HypertextLiteral = "~1.0.0"
+PlutoUI = "~0.7.80"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.10.9"
+julia_version = "1.10.11"
 manifest_format = "2.0"
-project_hash = "e7223c1a7ed85110e7a6918a8fa41e0f1158e7b8"
+project_hash = "5ee44924343ee7c02f4257d59ecbaca664a19aef"
 
 [[deps.AbstractPlutoDingetjes]]
-deps = ["Pkg"]
-git-tree-sha1 = "6e1d2a35f2f90a4bc7c2ed98079b2ba09c35b83a"
+git-tree-sha1 = "6c3913f4e9bdf6ba3c08041a446fb1332716cbc2"
 uuid = "6e696c72-6542-2067-7265-42206c756150"
-version = "1.3.2"
+version = "1.4.0"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
@@ -299,9 +349,15 @@ uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
 
 [[deps.ColorTypes]]
 deps = ["FixedPointNumbers", "Random"]
-git-tree-sha1 = "b10d0b65641d57b8b4d5e234446582de5047050d"
+git-tree-sha1 = "67e11ee83a43eb71ddc950302c53bf33f0690dfe"
 uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
-version = "0.11.5"
+version = "0.12.1"
+
+    [deps.ColorTypes.extensions]
+    StyledStringsExt = "StyledStrings"
+
+    [deps.ColorTypes.weakdeps]
+    StyledStrings = "f489334b-da3d-4c2e-b8f0-e476e12c162b"
 
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
@@ -334,25 +390,19 @@ version = "0.0.5"
 
 [[deps.HypertextLiteral]]
 deps = ["Tricks"]
-git-tree-sha1 = "7134810b1afce04bbc1045ca1985fbe81ce17653"
+git-tree-sha1 = "d1a86724f81bcd184a38fd284ce183ec067d71a0"
 uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
-version = "0.9.5"
+version = "1.0.0"
 
 [[deps.IOCapture]]
 deps = ["Logging", "Random"]
-git-tree-sha1 = "b6d6bfdd7ce25b0f9b2f6b3dd56b2673a66c8770"
+git-tree-sha1 = "0ee181ec08df7d7c911901ea38baf16f755114dc"
 uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
-version = "0.2.5"
+version = "1.0.0"
 
 [[deps.InteractiveUtils]]
 deps = ["Markdown"]
 uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
-
-[[deps.JSON]]
-deps = ["Dates", "Mmap", "Parsers", "Unicode"]
-git-tree-sha1 = "31e996f0a15c7b280ba9f76636b3ff9e2ae58c9a"
-uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
-version = "0.21.4"
 
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
@@ -363,15 +413,6 @@ version = "0.6.4"
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
 version = "8.4.0+0"
-
-[[deps.LibGit2]]
-deps = ["Base64", "LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
-uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
-
-[[deps.LibGit2_jll]]
-deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll"]
-uuid = "e37daf67-58a4-590a-8e99-b0245dd2ffc5"
-version = "1.6.4+0"
 
 [[deps.LibSSH2_jll]]
 deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
@@ -400,14 +441,11 @@ uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
-version = "2.28.2+1"
-
-[[deps.Mmap]]
-uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+version = "2.28.1010+0"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
-version = "2023.1.10"
+version = "2025.12.2"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
@@ -416,44 +454,17 @@ version = "1.2.0"
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
-version = "0.3.23+4"
-
-[[deps.Parsers]]
-deps = ["Dates", "PrecompileTools", "UUIDs"]
-git-tree-sha1 = "7d2f8f21da5db6a806faf7b9b292296da42b2810"
-uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
-version = "2.8.3"
-
-[[deps.Pkg]]
-deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
-uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
-version = "1.10.0"
+version = "0.3.23+5"
 
 [[deps.PlutoUI]]
-deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
-git-tree-sha1 = "d3de2694b52a01ce61a036f18ea9c0f61c4a9230"
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Downloads", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
+git-tree-sha1 = "fbc875044d82c113a9dee6fc14e16cf01fd48872"
 uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
-version = "0.7.62"
-
-[[deps.PrecompileTools]]
-deps = ["Preferences"]
-git-tree-sha1 = "5aa36f7049a63a1528fe8f7c3f2113413ffd4e1f"
-uuid = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
-version = "1.2.1"
-
-[[deps.Preferences]]
-deps = ["TOML"]
-git-tree-sha1 = "9306f6085165d270f7e3db02af26a400d580f5c6"
-uuid = "21216c6a-2e73-6563-6e65-726566657250"
-version = "1.4.3"
+version = "0.7.80"
 
 [[deps.Printf]]
 deps = ["Unicode"]
 uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
-
-[[deps.REPL]]
-deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
-uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
 
 [[deps.Random]]
 deps = ["SHA"]
@@ -471,9 +482,6 @@ version = "0.7.0"
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
 
-[[deps.Sockets]]
-uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
-
 [[deps.SparseArrays]]
 deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
 uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
@@ -489,29 +497,19 @@ deps = ["Artifacts", "Libdl", "libblastrampoline_jll"]
 uuid = "bea87d4a-7f5b-5778-9afe-8cc45184846c"
 version = "7.2.1+1"
 
-[[deps.TOML]]
-deps = ["Dates"]
-uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
-version = "1.0.3"
-
-[[deps.Tar]]
-deps = ["ArgTools", "SHA"]
-uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
-version = "1.10.0"
-
 [[deps.Test]]
 deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
 uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 
 [[deps.Tricks]]
-git-tree-sha1 = "6cae795a5a9313bbb4f60683f7263318fc7d1505"
+git-tree-sha1 = "311349fd1c93a31f783f977a71e8b062a57d4101"
 uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
-version = "0.1.10"
+version = "0.1.13"
 
 [[deps.URIs]]
-git-tree-sha1 = "cbbebadbcc76c5ca1cc4b4f3b0614b3e603b5000"
+git-tree-sha1 = "bef26fb046d031353ef97a82e3fdb6afe7f21b1a"
 uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
-version = "1.5.2"
+version = "1.6.1"
 
 [[deps.UUIDs]]
 deps = ["Random", "SHA"]
@@ -534,11 +532,6 @@ version = "5.11.0+0"
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
 version = "1.52.0+1"
-
-[[deps.p7zip_jll]]
-deps = ["Artifacts", "Libdl"]
-uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
-version = "17.4.0+2"
 """
 
 # ╔═╡ Cell order:
@@ -550,6 +543,8 @@ version = "17.4.0+2"
 # ╠═f5882dbd-a603-4df8-8a20-3b2b34f0cf84
 # ╠═4edba64f-4e71-4780-964a-d8635470a595
 # ╠═99246dda-6df5-4f3e-adeb-9a3976c4bb51
+# ╠═8664c054-1cf5-41bb-9f86-c1ab408b1110
+# ╠═72a735ab-41bd-4ef3-ab2a-a019d9608731
 # ╠═88d541c1-4e3f-4852-a58a-610a184f5e61
 # ╠═1310c8d2-16a6-4e94-8daa-3f3d9acc82d6
 # ╠═c132d10a-c408-4697-95ee-c54c65119aa3
@@ -562,6 +557,8 @@ version = "17.4.0+2"
 # ╠═4ca41aa3-45d3-4059-a2a6-edd4041a12af
 # ╠═600b676e-19aa-11ef-0a02-cd61e0a3dff3
 # ╠═48d03cad-0d61-4996-8458-ba39aa42efee
-# ╠═97b64bd7-49d0-4f7f-908d-06848abe6ab7
+# ╟─bd860523-ec6e-41f0-90da-6f8a5b627d9e
+# ╠═b681cfc2-a5f1-4d1f-942f-3847d8ddf58a
+# ╠═ddde2dcb-e280-4c7d-ae50-f624222e6a41
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
